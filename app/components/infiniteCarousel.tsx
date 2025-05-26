457a713c5a2487d6f52585852e79217f2e3db6e4
JMBoulos <jeanmarc8boulos@gmail.com>
2025-05-26 23:36:35 +0300

    Initialize Next.js with TypeScript, install and configure Tailwind CSS, and start development server -26/05/2025

---
+++ app/components/infiniteCarousel.tsx
@@ -0,0 +1,24 @@
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
+
+const InfiniteCarousel = ({
+  logos,
+}: {
+  logos: { src: StaticImageData; url: string }[]
+}) => {
+  return (
+    <div className="max-w-[800ox]">
+      <div className="scroll__inner">
+        {logos.map((logo, index) => {
+          return(
+            <Link key={index} href={logo.url}>
+              <Image src={logo.src} alt={`logo ${index + 1}`} height={100} />
+            </Link>
+          )
+        })}
+      </div>
+    </div>
+  )
+};
+
+export default InfiniteCarousel;
---
+++ app/page.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import InfiniteCarousel from "./components/infiniteCarousel";
+
+import logo_1 from "@/public/logos/american-apparel-logo-vector.svg";
+import logo_2 from "@/public/logos/asics-logo-vector-2.svg";
+import logo_3 from "@/public/logos/breguet-logo-vector.svg";
+import logo_4 from "@/public/logos/burberrys-of-london-logo-vector.svg";
+import logo_5 from "@/public/logos/dolce-gabbana-logo-vector.svg";
+import logo_6 from "@/public/logos/gucci-logo-vector.svg";
+import logo_7 from "@/public/logos/hermes-logo-vector.svg";
+import logo_8 from "@/public/logos/louis-vuitton-logo-vector.svg";
+import logo_9 from "@/public/logos/new-balance-logo-vector.svg";
+import logo_10 from "@/public/logos/omega-logo-vector.svg";
+
+const logos = [
+  { src: logo_1, url: "/" },
+  { src: logo_2, url: "/" },
+  { src: logo_3, url: "/" },
+  { src: logo_4, url: "/" },
+  { src: logo_5, url: "/" },
+  { src: logo_6, url: "/" },
+  { src: logo_7, url: "/" },
+  { src: logo_8, url: "/" },
+  { src: logo_9, url: "/" },
+  { src: logo_10, url: "/" },
+];
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <InfiniteCarousel logos={logos}/>
+    </main>
+  );
+}
