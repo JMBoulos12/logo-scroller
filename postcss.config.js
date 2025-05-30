7b754118d584d8d9cd81284bc3bcc3355499354c
JMBoulos <jeanmarc8boulos@gmail.com>
2025-05-30 19:10:35 +0300

    Updated the settings to have the focus on the logos only -30/05/2025

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
-const autoprefixer = require("autoprefixer");
-
-/* module.exports = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-    autoprefixer: {},
-  },
-}; */
-
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    "@tailwindcss/postcss": {},
     autoprefixer: {},
   },
 };
